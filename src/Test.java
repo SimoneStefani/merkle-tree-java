02f0f49d1e98cd47fbbac963f7c6b13b25f05b84
Simone Stefani <s.stefani95@gmail.com>
2018-02-10 22:24:12 +0100

    Refactor project for Gradle

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-
-    public static void main(String[] args) {
-        MerkleHash hash = MerkleHash.create("hello");
-        System.out.println(hash);
-        System.out.println(MerkleHash.create("hello").equals(hash));
-    }
-}
